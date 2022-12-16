2e5757fcd16425990fc3574cf25a1add3e64b74e
magn3054 <mdch20@student.aau.dk>
2022-12-16 23:49:11 +0100

    5 pages made for webpage

---
+++ videofeed.js
@@ -0,0 +1,15 @@
+function video() {
+    let video = document.querySelector("#videoElement");
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video:  true })
+            .then(function (stream) {
+                video.srcObject = stream; 
+            })
+            .catch (function (error) {
+                console.log("Something went wrong!")
+            })
+    } else {
+        console.log("getUserMedia not supported")
+    }
+}
